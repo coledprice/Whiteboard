620b8238ddacbbc53d8f885d4164c3c3d491073e
Cole Price <colevart@gmail.com>
2023-02-03 12:11:49 -0500

    less than 100

--- whiteboard.rb
+++ whiteboard.rb
@@ -1,11 +1,28 @@
 # Write a function that returns the sum of all numbers in a given array.
 
-def sum(numbers)
-  sum = 0
+# def sum(numbers)
+#   sum = 0
+#   numbers.each do |number|
+#     sum = sum + number
+#   end
+#   puts sum
+# end
+
+# sum([1, 2, 3, 4])
+
+# Given an array of numbers, write a function that returns a new array that contains all numbers from the original array that are less than 100.
+
+# Input: [99, 101, 88, 4, 2000, 50]
+# Output: [99, 88, 4, 50]
+
+def less_than_100(numbers)
+  new_array = []
   numbers.each do |number|
-    sum = sum + number
+    if number < 100
+      new_array << number
+    end
   end
-  puts sum
+  puts new_array
 end
 
-sum([1, 2, 3, 4])
+less_than_100([99, 101, 88, 4, 2000, 50])
