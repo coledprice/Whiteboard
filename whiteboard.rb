0eab709e56a7b23e3d204ba125f86012fe68c17b
Cole Price <colevart@gmail.com>
2023-02-03 12:16:21 -0500

    doubled

--- whiteboard.rb
+++ whiteboard.rb
@@ -15,14 +15,29 @@
 # Input: [99, 101, 88, 4, 2000, 50]
 # Output: [99, 88, 4, 50]
 
-def less_than_100(numbers)
+# def less_than_100(numbers)
+#   new_array = []
+#   numbers.each do |number|
+#     if number < 100
+#       new_array << number
+#     end
+#   end
+#   puts new_array
+# end
+
+# less_than_100([99, 101, 88, 4, 2000, 50])
+
+# Given an array of numbers, write a function that returns a new array whose values are the original array’s value doubled.
+
+# Input: [4, 2, 5, 99, -4]
+# Output: [8, 4, 10, 198, -8]
+
+def doubled(numbers)
   new_array = []
   numbers.each do |number|
-    if number < 100
-      new_array << number
-    end
+    new_array << number * 2
   end
   puts new_array
 end
 
-less_than_100([99, 101, 88, 4, 2000, 50])
+doubled([4, 2, 5, 99, -4])
