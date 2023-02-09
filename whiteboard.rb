ddf959b868d815cefab7c0b8b031c85cc087f4cd
Cole Price <colevart@gmail.com>
2023-02-09 13:26:44 -0500

    times 10 plus 30

--- whiteboard.rb
+++ whiteboard.rb
@@ -186,8 +186,16 @@
 
 # Write a function that takes in 3 numbers and returns the average (the sum divided by 3.0). Then run the function and print the result.
 
-def average(number1, number2, number3)
-  return (number1 + number2 + number3) / 3
+# def average(number1, number2, number3)
+#   return (number1 + number2 + number3) / 3
+# end
+
+# p average(2, 6, 7)
+
+# Write a function that takes in a number and returns the number times 10 plus 30. Then run the function and print the result.
+
+def times_10_plus_30(number)
+  return (number * 10) + 30
 end
 
-p average(2, 6, 7)
+p times_10_plus_30(3)
