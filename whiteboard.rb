9a6e29681ba19210141063d2daf1e490e9e7d6df
Cole Price <colevart@gmail.com>
2023-10-12 09:21:26 -0400

    two variables practice

--- whiteboard.rb
+++ whiteboard.rb
@@ -616,12 +616,24 @@
 
 # 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
 
-number = 10
+# number = 10
 
-if number < 10
-  p -1
-elsif number > 10
+# if number < 10
+#   p -1
+# elsif number > 10
+#   p 1
+# else number == 10 
+#   p 0
+# end
+
+# 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+
+number1 = 0
+number2 = 15
+
+if number1 < 10 && number2 < 10
   p 1
-else number == 10 
+else 
   p 0
-end
+end  
+
