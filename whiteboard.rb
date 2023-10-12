90f6aaa807972aad3f125a14899f2518d32d34f8
Cole Price <colevart@gmail.com>
2023-10-12 09:46:29 -0400

    1 p statement

--- whiteboard.rb
+++ whiteboard.rb
@@ -639,10 +639,23 @@
 
 # 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
 
-number = 9000
+# number = 9000
 
-if number > 9000
-  p 1
+# if number > 9000
+#   p 1
+# else
+#   p -1
+# end
+
+# 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
+
+number = 50
+if number < 10 
+  p 9
+elsif number < 20
+  p 19
+elsif number < 30
+  p 29
 else
   p -1
-end
+end 
