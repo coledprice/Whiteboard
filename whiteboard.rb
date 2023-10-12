826c19510d105f393c2b88fac6c83e0cd8a5c569
Cole Price <colevart@gmail.com>
2023-10-12 09:16:57 -0400

    more practice

--- whiteboard.rb
+++ whiteboard.rb
@@ -607,9 +607,21 @@
 
 # 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
 
+# number = 10
+#  if number == 10
+#   p 0
+#  else 
+#   p -1
+#  end
+
+# 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+
 number = 10
- if number == 10
-  p 0
- else 
+
+if number < 10
   p -1
- end
+elsif number > 10
+  p 1
+else number == 10 
+  p 0
+end
