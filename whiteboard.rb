2b436b9b6c290862b9314c63286664bd94901bae
Cole Price <colevart@gmail.com>
2023-10-12 10:05:13 -0400

    more practice

--- whiteboard.rb
+++ whiteboard.rb
@@ -694,11 +694,27 @@
 
 # 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
 
-number1 = -1
-number2 = 5
+# number1 = -1
+# number2 = 5
 
-if number1 < 0 && number2 > 0
-  p 1 
+# if number1 < 0 && number2 > 0
+#   p 1 
+# else 
+#   p 0
+# end
+
+# 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
+
+number = 60
+if number > 80
+  p 5
+elsif
+  number > 60
+  p 4
+elsif number > 40
+  p 3
+elsif number > 20
+  p 2
 else 
-  p 0
+  p 1
 end
