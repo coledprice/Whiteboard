9b2c2e211a498bd7224fdf95e0486a57c3833be1
Cole Price <colevart@gmail.com>
2023-10-12 09:56:11 -0400

    condition

--- whiteboard.rb
+++ whiteboard.rb
@@ -682,3 +682,12 @@
 
 # 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
 
+number = 99 
+if number == 100
+  p 100
+elsif
+  number == 99 
+  p 99
+else
+  p 0
+end
