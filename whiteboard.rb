c7a5c5426bc38d7c91c67866913dd3d0e0607397
Cole Price <colevart@gmail.com>
2023-10-12 09:53:45 -0400

    pract

--- whiteboard.rb
+++ whiteboard.rb
@@ -662,11 +662,23 @@
 
 # 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
 
-number1 = 5
-number2 = 5
+# number1 = 5
+# number2 = 5
 
-if number1 > 10 || number2 > 10
-  p 100
-else
-  p -100
-end
+# if number1 > 10 || number2 > 10
+#   p 100
+# else
+#   p -100
+# end
+
+# 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
+
+# number = -5
+# if number < 0
+#   p 1776
+# else
+#   p 1979
+# end
+
+# 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
+
