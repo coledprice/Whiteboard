66e8e0c7beb964c41ce55edec716c14e05985e45
Cole Price <colevart@gmail.com>
2023-10-12 09:39:57 -0400

    over 9000

--- whiteboard.rb
+++ whiteboard.rb
@@ -628,12 +628,21 @@
 
 # 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
 
-number1 = 0
-number2 = 15
+# number1 = 0
+# number2 = 15
 
-if number1 < 10 && number2 < 10
+# if number1 < 10 && number2 < 10
+#   p 1
+# else 
+#   p 0
+# end  
+
+# 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
+
+number = 9000
+
+if number > 9000
   p 1
-else 
-  p 0
-end  
-
+else
+  p -1
+end
