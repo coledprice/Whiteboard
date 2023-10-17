bad710fba2b51841d1dc9d980a15695d1ccd2f8a
Cole Price <colevart@gmail.com>
2023-10-17 12:29:59 -0400

    while loops

--- whiteboard.rb
+++ whiteboard.rb
@@ -878,11 +878,94 @@
 
 # 1. Write a while loop to print the numbers 1 through 10.
 
-index = 1
+# index = 1
 
-while index < 11
-  puts index
-index += 1
+# while index < 11
+#   puts index
+# index += 1
+# end
+
+# 2. Write a while loop that prints the word "hello" 5 times.
+
+# index = 1
+# while index < 6
+#   puts "hello"
+#   index += 1
+# end
+
+# 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
+
+# while true
+#   puts "enter a word: "
+#   word = gets.chomp
+#   if word == "stop"
+#     break
+#   end
+# end
+
+# 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+
+# index = 0 
+#  while index <= 100
+#   puts index
+#   index += 5
+#  end
+
+
+# 5. Write a while loop that prints the number 9000 ten times.
+
+# number = 1
+
+# while number <= 10
+#   puts 9000
+#   number += 1
+# end
+
+
+# 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
+
+# while true
+#   puts "enter a number: "
+#   number = gets.chomp
+#   if number.to_i == 10
+#     break
+#   end
+# end
+
+# 7. Write a while loop that prints the numbers 50 to 70.
+
+# number = 50
+# while number <= 70
+#   puts number
+#   number += 1
+# end
+
+# 8. Write a while loop that prints the phrase "Around the world" 144 times.
+
+# index = 1
+# while index <= 144
+#   puts "Around the world"
+#   index += 1
+# end
+
+# 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
+
+# while true 
+#   puts "enter a word: "
+#   word = gets.chomp
+#   if word.length > 5
+#     break
+#   end
+# end
+
+# 10. Write a while loop that prints the even numbers from 2 to 40.
+
+index = 2 
+while index <= 40
+  if index.even?
+    puts index
+  end
+  index += 1
 end
 
 
