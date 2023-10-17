881dec162fcf079c7faeba0e6e6c097749a288a2
Cole Price <colevart@gmail.com>
2023-10-17 11:36:45 -0400

    reverse

--- whiteboard.rb
+++ whiteboard.rb
@@ -801,7 +801,31 @@
 
 # 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
 
-puts "please type in a word"
-word = gets.chomp
+# puts "please type in a word"
+# word = gets.chomp
 
-puts word.upcase
+# puts word.upcase
+
+# 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+
+# puts "please enter a number"
+
+# number = gets.chomp
+# if number.to_i > 100
+#   puts "That's a big number"
+# end
+
+# 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
+
+# puts "please enter two numbers"
+# number1 = gets.chomp
+# number2 = gets.chomp
+
+# puts number1.to_i + number2.to_i 
+
+# 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
+
+puts "please enter a word"
+
+word = gets.chomp
+puts word.reverse
