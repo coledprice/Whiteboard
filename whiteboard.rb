be05a5e283939a34a789f26d393d74bd087f579c
Cole Price <colevart@gmail.com>
2023-10-18 09:48:52 -0400

    functions

--- whiteboard.rb
+++ whiteboard.rb
@@ -1129,9 +1129,88 @@
 
 # 10. Make a hash to store a shirt's brand, color, and size. Then print each attribute on separate lines.
 
-shirt = {brand: "hanes", color: "white", size: "Large"}
+# shirt = {brand: "hanes", color: "white", size: "Large"}
 
-p shirt[:brand]
-p shirt[:color]
-p shirt[:size]
+# p shirt[:brand]
+# p shirt[:color]
+# p shirt[:size]
 
+# 1. Write a function that takes in a number and returns the number times two. Then run the function and print the result.
+
+# def doubled(number)
+#   return number * 2
+# end
+
+# p doubled(4)
+
+# 2. Write a function that takes in a string and returns the string with all capital letters. Then run the function and print the result.
+
+# def capitalize(string)
+#   return string.upcase
+# end
+
+# p capitalize("apple")
+
+# 3. Write a function that takes in two numbers and returns the first number subtracted by the second. Then run the function and print the result.
+
+# def subtract(number1, number2)
+#   return number1 - number2
+# end
+
+# p subtract(6, 4)
+
+# 4. Write a function that takes in a number and returns the number times itself. Then run the function and print the result.
+
+# def multiply(number)
+#   return number * number
+# end
+
+# p multiply(5)
+
+# 5. Write a function that takes in a string and returns the first letter of the string. Then run the function and print the result.
+
+# def first_letter(string)
+#   return string[0]
+# end
+
+# p first_letter("orange")
+
+# 6. Write a function that takes in three strings and returns a string that combines all three strings with spaces in between. Then run the function and print the result.
+
+# def combine(string1, string2, string3)
+#   return string1 + " " + string2 + " " + string3
+# end
+
+# p combine("apple", "orange", "pear")
+
+# 7. Write a function that takes in a number and returns the number as a string. Then run the function and print the result.
+
+# def number_becomes_string(number)
+#   return number.to_s
+# end
+
+# p number_becomes_string(1)
+
+# 8. Write a function that takes in a string and returns the string repeated 5 times. Then run the function and print the result.
+
+# def repeat_five(string)
+#   return string * 5
+# end
+
+# p repeat_five("dog")
+
+# 9. Write a function that takes in 3 numbers and returns the average (the sum divided by 3.0). Then run the function and print the result.
+
+# def average(number1, number2, number3)
+#   return (number1 + number2 + number3) / 3
+# end
+
+# p average(10, 1, 1)
+
+# 10. Write a function that takes in a number and returns the number times 10 plus 30. Then run the function and print the result.
+
+def times_plus(number)
+  return (number * 10) + 30
+end
+
+p times_plus(5)
